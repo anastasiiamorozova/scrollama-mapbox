b7f738ae1a9f6602cf7c502b379d4b38a0581c51
anastasiiamorozova <107928589+anastasiiamorozova@users.noreply.github.com>
2024-07-22 12:24:21 -0400

    update

--- config.js
+++ config.js
@@ -16,34 +16,21 @@ var config = {
     //footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
-            id: 'okhtyrka',
+            id: 'intro',
             alignment: 'left',
-            hidden: false,
+            hidden: true,
             title: 'Охтирка',
             //image: 'sorry.png',
             //description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             location: {
-                center: [34.88819, 50.30709],
-zoom: 13.03,
-pitch: 45.00,
+                center: [34.07056, 51.38996],
+zoom: 6.97,
+pitch: 25.00,
 bearing: 0.00
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
             callback: '',
-            onChapterEnter: [
-                {
-                    layer: 'intro',
-                    opacity: 1,
-                    duration: 5000
-                }
-            ],
-            onChapterExit: [
-                {
-                    layer: 'okhtyrka',
-                    opacity: 1
-                }
-            ]
         },
         {
             id: 'shostka',
@@ -51,11 +38,11 @@ bearing: 0.00
             hidden: false,
             title: 'Шостка',
             //image: './path/to/image/source.png',
-            description: 'Copy these sections to add to your story.',
+            description: 'В Шостці є х укриттів, останнє оновлення. Покрито % тер',
             location: {
                 center: [33.48702, 51.85600],
-zoom: 12.46,
-pitch: 45.00,
+zoom: 13,
+pitch: 25.00,
 bearing: 0.00
                 // flyTo additional controls-
                 // These options control the flight curve, making it move
@@ -76,19 +63,35 @@ bearing: 0.00
                 opacity: 1,
                 duration: 5000}]
         },
-        
         {
             id: 'konotop',
             alignment: 'left',
             hidden: false,
             title: 'Конотоп',
+            //image: 'sorry.png',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            location: {
+                center: [33.20471, 51.23985],
+zoom: 12.03,
+pitch: 25.00,
+bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+        },
+        {
+            id: 'Romny',
+            alignment: 'right',
+            hidden: false,
+            title: 'Ромни',
             /*image: './path/to/image/source.png',*/
             description: 'Copy these sections to add to your story.',
             location: {
-                center: [-74.04, 40.74],
-                zoom: 10,
-                pitch: 0,
-                bearing: 0
+                center: [33.48682, 50.74290],
+zoom: 13.01,
+pitch: 25.00,
+bearing: 0.00
             },
             //mapAnimation: 'flyTo',
             rotateAnimation: false,
@@ -97,16 +100,35 @@ bearing: 0.00
             onChapterExit: []
         },
         {
-            id: 'staten island',
-            alignment: 'fully',
+            id: 'sumy',
+            alignment: 'left',
             hidden: false,
-            title: 'Third Title',
+            title: 'Sumy',
             //image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: [-74.12432, 40.56992],
-zoom: 11.35,
-pitch: 45.00,
+                center: [34.80553, 50.91211],
+zoom: 12.49,
+pitch: 25.00,
+bearing: 0.00
+            },
+            //mapAnimation: 'flyTo',
+            //rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'okhtyrka',
+            alignment: 'left',
+            hidden: false,
+            title: 'Охтирка',
+            //image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            location: {
+                center: [34.89727, 50.31056],
+zoom: 12.05,
+pitch: 25.00,
 bearing: 0.00
             },
             //mapAnimation: 'flyTo',
