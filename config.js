9e402344f4be8c0617cac1d12a1ee9ef885a0aa1
anastasiiamorozova <107928589+anastasiiamorozova@users.noreply.github.com>
2024-07-23 14:30:49 -0400

    translated

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ var config = {
     theme: 'dark',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
-    title: 'Укриття поруч?',
-    subtitle: 'Кордон.Медіа провели дослідження, наскільки районні центри Сумської області забезпечені укриттями. Ми порахували, з яких вулиць можна дістатися до найближчого укриття за 5 хвилин.',
+    title: 'Are you safe during the air attack?',
+    subtitle: 'I analyzed the 5 biggest cities in the Sumy region of Ukraine. I marked the streets from which you can reach the nearest shelter within 5 minutes in green. It turned out that in all but one city more that 50% of the area is unsafe.',
     byline: 'Anastasiia Morozova',
     //footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
@@ -36,9 +36,9 @@ bearing: 0.00
             id: 'shostka',
             alignment: 'right',
             hidden: false,
-            title: 'Шостка',
+            title: 'Shostka',
             //image: './path/to/image/source.png',
-            description: 'В Шостці є х укриттів, останнє оновлення. Покрито % тер',
+            description: 'There are 59 shelters in Shostka. However, they are mostly located in the city centre because a major part of the city is private housing.',
             location: {
                 center: [33.48702, 51.85600],
 zoom: 13,
@@ -67,9 +67,9 @@ bearing: 0.00
             id: 'konotop',
             alignment: 'left',
             hidden: false,
-            title: 'Конотоп',
+            title: 'Konotop',
             //image: 'sorry.png',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            description: 'In Konotop the number of shelters is even smaller – 47. Which makes it the least safe in the region.',
             location: {
                 center: [33.20471, 51.23985],
 zoom: 12.03,
@@ -84,9 +84,9 @@ bearing: 0.00
             id: 'Romny',
             alignment: 'right',
             hidden: false,
-            title: 'Ромни',
+            title: 'Romny',
             /*image: './path/to/image/source.png',*/
-            description: 'Copy these sections to add to your story.',
+            description: 'Although in Romny the local authorities report to have 67 shelters, some of them are located outside the city in rural area.',
             location: {
                 center: [33.48682, 50.74290],
 zoom: 13.01,
@@ -105,7 +105,7 @@ bearing: 0.00
             hidden: false,
             title: 'Sumy',
             //image: './path/to/image/source.png',
-            description: 'Copy these sections to add to your story.',
+            description: 'Sumy is the biggest city in the region, therefore it has the biggest number of shelters. However, it has been often reported by the journalist that during air raid alert some shelters are closed.',
             location: {
                 center: [34.80553, 50.91211],
 zoom: 12.49,
@@ -122,9 +122,9 @@ bearing: 0.00
             id: 'okhtyrka',
             alignment: 'left',
             hidden: false,
-            title: 'Охтирка',
+            title: 'Okhtyrka',
             //image: './path/to/image/source.png',
-            description: 'Copy these sections to add to your story.',
+            description: 'Okhtyrka has been partially occupied as the war broke out. Today it counts 107 shelters, which are located mostly in the city center.',
             location: {
                 center: [34.89727, 50.31056],
 zoom: 12.05,
@@ -141,7 +141,7 @@ bearing: 0.00
             id: 'search',
             alignment: 'right',
             hidden: false,
-            title: 'Подивіться чи на вашій вулиці є укриття',
+            title: 'See if your street is in the safe area.',
             //image: './path/to/image/source.png',
             //description: 'Copy these sections to add to your story.',
             location: {
